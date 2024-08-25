7494355485694696b4633730c864f80644d76372
Ayodele Odugbile <udumary1993@gmail.com>
2024-08-25 15:24:17 +0100

    adding code to python.py

--- Project.py
+++ Project.py
@@ -225,6 +225,232 @@ print("Hello {}, your lucky number is {}".format(name, number))
 name = "Manny"
 print("Your lucky number is {number}, {name}.".format(name=name, number=len(name)*3))
 
+price = 7.5
+with_tax = price * 1.09
+print(price, with_tax)
+print("Base price: ${:.2f}. With Tax: ${:.2f}".format(price, with_tax))
+
+def to_celsius(x):
+  return (x-32)*5/9
+
+for x in range(0,101,10):
+  print("{:>3} F | {:>6.2f} C".format(x, to_celsius(x)))
+  
+
+# This function accepts a given string and checks each character of 
+# the string to see if it is a letter or not. If the character is a
+# letter, that letter is added to the end of the string variable
+# "forwards" and to the beginning of the string variable "backwards".
+def mirrored_string(my_string):
+
+    # Two variables are initialized as string data types using empty 
+    # quotes. The variable "forwards" will hold the "my_string"
+    # minus any character that is not a letter. The "backwards" 
+    # variable will hold the same letters as "forwards", but in  
+    # in reverse order.
+    forwards = ""
+    backwards = ""
+
+    # The for loop iterates through each character of the "my_string"
+    for character in my_string:
+
+        # The if-statement checks if the character is not a space.
+        if character.isalpha():
+
+            # If True, the body of the loop adds the character to the
+            # to the end of "forwards" and to the front of
+            # "backwards". 
+            forwards += character
+            backwards = character + backwards
+
+        # If False (meaning the character is not a letter), no action
+        # is needed. This coding approach results prevents any 
+        # non-alphabetical characters from being written to the
+        # "forwards" and "backwards" variables. The for loop will 
+        # restart until all characters in "my_string" have been
+        # processed.
+        
+    # The final if-statement compares the "forwards" and "backwards"
+    # strings to see if the letters are the same both forwards and
+    # backwards. Since Python is case sensitive, the two strings will 
+    # need to be converted to use the same case for this comparison. 
+    if forwards.lower() == backwards.lower():
+        return True
+    return False
+ 
+print(mirrored_string("12 Noon")) # Should be True
+print(mirrored_string("Was it a car or cat I saw")) # Should be False
+print(mirrored_string("'eve, Madam Eve")) # Should be True
+
+# This function converts measurement equivalents. Output is formatted 
+# as, "x ounces equals y pounds", with y limited to 2 decimal places. 
+def convert_weight(ounces):
+
+    # Conversion formula: 1 pound = 16 ounces
+    pounds = ounces/16 
+    
+    # The result is composed using the .format() method. There are two
+    # placeholders in the string: the first is for the "ounces" 
+    # variable and the second is for the "pounds" variable. The second
+    # placeholder formats the float result of the conversion 
+    # calculation to be limited to 2 decimal places.
+    result = "{} ounces equals {:.2f} pounds".format(ounces,pounds)
+    return result
+
+
+print(convert_weight(12)) # Should be: 12 ounces equals 0.75 pounds
+print(convert_weight(50.5)) # Should be: 50.5 ounces equals 3.16 pounds
+print(convert_weight(16)) # Should be: 16 ounces equals 1.00 pounds
+
+# This function generates a username using the first 3 letters of a
+# user’s last name plus their birth year. 
+def username(last_name, birth_year):
+    
+    # The .format() method will use the first 3 letters at index 
+    # positions [0,1,2] of the "last_name" variable for the first
+    # {} placeholder. The second {} placeholder concatenates the user’s
+    #  "birth_year" to that string to form a new string username.
+    return("{}{}".format(last_name[0:3],birth_year))
+
+
+print(username("Ivanov", "1985")) 
+# Should display "Iva1985" 
+print(username("Rodríguez", "2000")) 
+# Should display "Rod2000" 
+print(username("Deng", "1991")) 
+# Should display "Den1991"
+
+
+# This function checks a given schedule entry for an old date and, if 
+# found, the function replaces it with a new date. 
+def replace_date(schedule, old_date, new_date):
+
+    # Check if the given "old_date" appears at the end of the given 
+    # string variable "schedule". 
+    if schedule.endswith(old_date):
+
+        # If True, the body of the if-block will run. The variable "p" is
+        # used to hold the slicing index position. The len() function
+        # is used to measure the length of the string "old_date".
+        p = len(old_date)
+
+        # The "new_schedule" string holds the updated string with the 
+        # old date replaced by the new date. The schedule[:-p] part of 
+        # the code trims the "old_date" substring from "schedule" 
+        # starting at the final index position (or right-side) counting
+        # towards the left the same number of index positions as 
+        # calculated from len(old_date). Then, the code schedule[-p:]
+        # starts the indexing position at the slot where the first
+        # character of the "old_date" used to be positioned. The 
+        # .replace(old_date, new_date) code inserts the "new_date" into
+        # the position where the "old_date" used to exist.  
+        new_schedule = schedule[:-p] + schedule[-p:].replace(old_date, new_date)
+
+        # Returns the schedule with the new date.
+        return new_schedule
+        
+    # If the schedule does not end with the old date, then return the
+    # original sentence without any modifications.
+    return schedule
+ 
+ 
+print(replace_date("Last year’s annual report will be released in March 2023", "2023", "2024")) 
+# Should display "Last year’s annual report will be released in March 2024"
+print(replace_date("In April, the CEO will hold a conference", "April", "May")) 
+# Should display "In April, the CEO will hold a conference"
+print(replace_date("The convention is scheduled for October", "October", "June")) 
+# Should display "The convention is scheduled for June"
+
+def convert_distance(miles):
+    km = miles * 1.6 
+    result = "{} miles equals {} km".format(miles, km)
+    return result
+
+
+print(convert_distance(12))  # Should be: 12 miles equals 19.2 km
+print(convert_distance(5.5)) # Should be: 5.5 miles equals 8.8 km
+print(convert_distance(11))  # Should be: 11 miles equals 17.6 km
+
+def nametag(first_name, last_name):
+    return "{} {}.".format(first_name, last_name[0])
+
+
+print(nametag("Jane", "Smith")) 
+# Should display "Jane S." 
+print(nametag("Francesco", "Rinaldi")) 
+# Should display "Francesco R." 
+print(nametag("Jean-Luc", "Grand-Pierre")) 
+# Should display "Jean-Luc G." 
+
+def replace_ending(sentence, old, new):
+    # Check if the old substring is at the end of the sentence 
+    if sentence.endswith(old):
+        # Using i as the slicing index, combine the part
+        # of the sentence up to the matched string at the 
+        # end with the new string
+        i = len(sentence) - len(old)
+        new_sentence = sentence[:i] + new
+        return new_sentence
+
+    # Return the original sentence if there is no match 
+    return sentence
+
+print(replace_ending("It's raining cats and cats", "cats", "dogs")) 
+# Should display "It's raining cats and dogs"
+print(replace_ending("She sells seashells by the seashore", "seashells", "donuts")) 
+# Should display "She sells seashells by the seashore"
+print(replace_ending("The weather is nice in May", "may", "april")) 
+# Should display "The weather is nice in May"
+print(replace_ending("The weather is nice in May", "May", "April")) 
+# Should display "The weather is nice in April"
+
+def is_palindrome(input_string):
+    # Two variables are initialized as string data types using empty 
+    # quotes: "reverse_string" to hold the "input_string" in reverse
+    # order and "new_string" to hold the "input_string" minus the 
+    # spaces between words, if any are found.
+    new_string = ""
+    reverse_string = ""
+
+    # Complete the for loop to iterate through each letter of the
+    # "input_string"
+    for letter in input_string:
+
+        # The if-statement checks if the "letter" is not a space.
+        if letter != " ":
+
+            # If True, add the "letter" to the end of "new_string" and
+            # to the front of "reverse_string". If False (if a space
+            # is detected), no action is needed. Exit the if-block.
+            new_string += letter
+            reverse_string = letter + reverse_string
+
+    # Complete the if-statement to compare the "new_string" to the
+    # "reverse_string". Remember that Python is case-sensitive when
+    # creating the string comparison code. 
+    if new_string.lower() == reverse_string.lower():
+
+        # If True, the "input_string" contains a palindrome.
+        return True
+	
+    # Otherwise, return False.
+    return False
+
+
+print(is_palindrome("Never Odd or Even")) # Should be True
+print(is_palindrome("abc")) # Should be False
+print(is_palindrome("kayak")) # Should be True
+
+def convert_distance(miles):
+    km = miles * 1.6 
+    result = "{} miles equals {:.1f} km".format(miles, km)
+    return result
+
+
+print(convert_distance(12))  # Should be: 12 miles equals 19.2 km
+print(convert_distance(5.5)) # Should be: 5.5 miles equals 8.8 km
+print(convert_distance(11))  # Should be: 11 miles equals 17.6 km
+
 
 
 
